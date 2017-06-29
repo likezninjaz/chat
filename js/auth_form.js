389c066d4babcde9d73d440e62ea5059544da070
likezninjaz <estrueall@gmail.com>
2017-06-30 00:29:13 +0400

    Added registration tab on form, changed styles for forms

---
+++ js/auth_form.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+	var $tabAuth=$('.form__tabs__auth'),
+		$tabReg=$('.form__tabs__reg'),
+		$formAuth=$('.form__auth'),
+		$formReg=$('.form__reg');
+
+	$tabReg.click(function() {
+		$(this).css({
+			"background-color":"#4caf50",
+			"color":"white",
+			"border-radius":"5px 5px 0 0"
+		});
+
+		$tabAuth.css({
+			"background-color":"white",
+			"color":"#90a4ae"
+		});
+
+		$formReg.show();
+		$formAuth.hide();
+	});
+
+	$tabAuth.click(function() {
+		$(this).css({
+			"background-color":"#4caf50",
+			"color":"white",
+			"border-radius":"5px 5px 0 0"
+		});
+
+		$tabReg.css({
+			"background-color":"white",
+			"color":"#90a4ae"
+		})
+
+		$formAuth.show();
+		$formReg.hide();
+	});
+});
